7eccae3e7bb6b8531193a671d5c0951600974c9e
Franklin Foko <fokofranklin47@gmail.com>
2023-09-10 16:05:58 +0100

    labs-2 OK

--- labs-2/test.tf
+++ labs-2/test.tf
@@ -1,1 +1,11 @@
-provider "k8s" {}
+# kubernetes provider version 2.23.0
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "my-context"
+}
+
+resource "kubernetes_namespace" "example" {
+  metadata {
+    name = "my-first-namespace"
+  }
+}
