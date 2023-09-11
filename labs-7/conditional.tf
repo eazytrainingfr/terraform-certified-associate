d98d343f5970c9ed3a8231526a1c1fa1e99efa70
Franklin Foko <fokofranklin47@gmail.com>
2023-09-11 16:05:09 +0100

    labs-7 OK

--- labs-7/conditional.tf
+++ labs-7/conditional.tf
@@ -1,3 +1,13 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
   region     = "us-east-1"
   access_key = "PUT YOUR OWN"
@@ -5,15 +15,17 @@ provider "aws" {
 }
 
 resource "aws_instance" "dev" {
-   ami = "ami-012cc038cc685a0d7"
-   instance_type = "t2.micro"
-   count = var.istest == true ? 3 : 0
+  ami           = "ami-0f34c5ae932e6f0e4"
+  instance_type = "t2.micro"
+  count         = var.istest == true ? 3 : 0
 }
 
 resource "aws_instance" "prod" {
-   ami = "ami-012cc038cc685a0d7"
-   instance_type = "t2.large"
-   count = var.istest == false ? 1 : 0
+  ami           = "ami-0f34c5ae932e6f0e4"
+  instance_type = "t2.micro"
+  count         = var.istest == false ? 1 : 0
 }
 
-variable "istest" {}
+variable "istest" {
+
+}
