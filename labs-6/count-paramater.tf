4320997b55ad5511f58dcdc1b439d99cfaca42bd
Franklin Foko <fokofranklin47@gmail.com>
2023-09-11 15:49:58 +0100

    labs-6 OK

--- labs-6/count-paramater.tf
+++ labs-6/count-paramater.tf
@@ -1,11 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
   region     = "us-east-1"
   access_key = "PUT YOUR OWN"
   secret_key = "PUT YOUR OWN"
 }
 
-resource "aws_instance" "instance-1" {
-   ami = "ami-012cc038cc685a0d7"
-   instance_type = "t2.micro"
-   count = 3
+resource "aws_instance" "myec2" {
+  ami           = "ami-0f34c5ae932e6f0e4"
+  instance_type = "t2.micro"
+  count = 3
 }
