5c012604df3634a9b07e5acce96277d387787355
Franklin Foko <fokofranklin47@gmail.com>
2024-01-18 16:40:37 +0100

    add tp azure labs 17 not ok

---
+++ TP Azure/labs-17/import.tf
@@ -0,0 +1,19 @@
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "import_rg" {
+  name     = "importrg"
+  location = "France Central"
+}
