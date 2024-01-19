d957c5e6dc9973fd57e73ce8b1882c2db4ed0e38
Franklin FOKO <fokofranklin47@gmail.com>
2024-01-19 01:07:54 +0000

    tp azure labs 19 ok

--- TP Azure/labs-19/providers.tf
+++ TP Azure/labs-19/providers.tf
@@ -16,4 +16,9 @@ provider "azurerm" {
 provider "azurerm" {
     alias = "azurerm2"
   features {}
+
+  subscription_id   = "<azure_subscription_id>"
+  tenant_id         = "<azure_subscription_tenant_id>"
+  client_id         = "<service_principal_appid>"
+  client_secret     = "<service_principal_password>"
 }
